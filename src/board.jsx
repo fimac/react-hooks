bd53c9b64817243b5daae9f1f6ac6febdc0954d3
fimac <fimccawley@gmail.com>
2020-07-01 15:15:37 +1000

    formatted board game index files
    heres another line in the commit message

--- src/board.jsx
+++ src/board.jsx
@@ -2,43 +2,37 @@ import React from "react";
 import "./index.css";
 
 const Square = (props) => {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
-const Board = ({squares, onClick}) => {
-    const renderSquare = (i) => {
-        return (
-          <Square
-            value={squares[i]}
-            onClick={() => onClick(i)}
-          />
-        );
-      }
+const Board = ({ squares, onClick }) => {
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+  };
 
-      return (
-        <div>
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-          </div>
-        </div>
-      );
-
-}
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;
--- src/game.jsx
+++ src/game.jsx
@@ -1,98 +1,95 @@
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./index.css";
-import Board from './board'
+import Board from "./board";
 
-const  calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-  
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    return null;
   }
-
+  return null;
+};
+// Adding some comments in the game component
 
 const Game = () => {
-    const [stepNumber, setStepNumber] = useState(0)
-    const [xIsNext, setXIsNext] = useState(true)
-    const [history, setHistory] =useState([
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+    },
+  ]);
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const handleClick = (i) => {
+    const historyCopy = history.slice(0, stepNumber + 1);
+    const current = historyCopy[historyCopy.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+
+    setHistory(
+      historyCopy.concat([
         {
-          squares: Array(9).fill(null),
+          squares: squares,
         },
       ])
+    );
+    setStepNumber(historyCopy.length);
+    setXIsNext(!xIsNext);
+  };
 
-      const jumpTo = (step) => {
-          setStepNumber(step)
-          setXIsNext((step % 2) === 0)
-       
-      }
-    
-      const handleClick = (i) => {
-        const historyCopy = history.slice(0, stepNumber + 1)
-        const current = historyCopy[historyCopy.length - 1]
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = xIsNext ? "X" : "O";
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
 
-        setHistory(historyCopy.concat([
-            {
-              squares: squares
-            }
-          ]))
-          setStepNumber(historyCopy.length)
-          setXIsNext(!xIsNext)
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
 
-      }
+    return (
+      <li key={move}>
+        {" "}
+        <button onClick={() => jumpTo(move)}>{desc} </button>
+      </li>
+    );
+  });
 
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
-      const current = history[stepNumber]
-      const winner = calculateWinner(current.squares);
-  
-      const moves = history.map((step, move) => {
-        const desc = move ? 'Go to move #' + move : 'Go to game start'
-  
-        return (
-          <li key={move}> <button onClick={() => jumpTo(move)}>{desc} </button></li>
-        )
-      })
-      
-      let status;
-      if (winner) {
-        status = "Winner: " + winner;
-      } else {
-        status = "Next player: " + (xIsNext ? "X" : "O");
-      }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
 
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={(i) => handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
-      );
-}
-
-  export default Game
-  
+export default Game;
